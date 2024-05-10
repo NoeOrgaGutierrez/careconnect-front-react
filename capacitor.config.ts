c54706a18f9d1395c12b475d3f005567654ff9fe
Noé Orga Gutiérrez <noeorgagutierrezcv@gmail.com>
2024-05-10 08:51:50 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'careconnect-front-react',
+  webDir: 'dist'
+};
+
+export default config;
