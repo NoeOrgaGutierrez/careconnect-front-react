edc0fa65ed78582ab00a7c504b1b8af6fa88e4f2
A26644 <noeorgagutierrezcv@gmail.com>
2024-05-12 20:32:21 +0200

    Test for android studio

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,33 +1,35 @@
 import {
-  IonButtons,
-  IonContent,
-  IonHeader,
-  IonTitle,
-  IonToolbar,
-  IonButton,
-  IonIcon,
-  IonRouterLink,
-} from "@ionic/react";
-import { personCircleOutline } from "ionicons/icons";
-import React from "react";
+	IonButtons,
+	IonContent,
+	IonHeader,
+	IonTitle,
+	IonToolbar,
+	IonButton,
+	IonIcon,
+	IonRouterLink
+} from '@ionic/react'
+import { personCircleOutline } from 'ionicons/icons'
+import React from 'react'
+import { Grid } from '@mui/material'
 const Communities: React.FC = () => {
-  return (
-    <>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle slot="start">Landing</IonTitle>
-          <IonButtons slot="end">
-            <IonButton size="large">
-              <IonRouterLink routerLink="/login">
-                <IonIcon icon={personCircleOutline} />
-              </IonRouterLink>
-            </IonButton>
-          </IonButtons>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent></IonContent>
-    </>
-  );
-};
+	return (
+		<>
+			<Grid></Grid>
+			<IonHeader>
+				<IonToolbar>
+					<IonTitle slot='start'>Landing</IonTitle>
+					<IonButtons slot='end'>
+						<IonButton size='large'>
+							<IonRouterLink routerLink='/login'>
+								<IonIcon icon={personCircleOutline} />
+							</IonRouterLink>
+						</IonButton>
+					</IonButtons>
+				</IonToolbar>
+			</IonHeader>
+			<IonContent></IonContent>
+		</>
+	)
+}
 
-export default Communities;
+export default Communities
