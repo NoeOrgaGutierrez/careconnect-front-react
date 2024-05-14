93b577713b7736f9e3b7e02a6d2166fcc835e5e7
Flateric2 <Pablofebe05@gmail.com>
2024-05-14 18:13:29 +0200

    Update Associations.tsx

--- src/components/associations/Associations.tsx
+++ src/components/associations/Associations.tsx
@@ -1,4 +1,6 @@
+import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
+import axios from "axios";
 import {
   IonButtons,
   IonContent,
@@ -7,9 +9,34 @@ import {
   IonTitle,
   IonToolbar,
 } from "@ionic/react";
+import { Card, CardContent, CardActions, Button, Typography, CardMedia } from "@mui/material";
+
+interface Association {
+  id: number;
+  name: string;
+  miniDescription: string;
+  description: string;
+  logo: string;
+  banner: string;
+}
 
 const Associations: React.FC<{ name: string }> = ({ name }) => {
   const location = useLocation();
+  const [associations, setAssociations] = useState<Association[]>([]);
+
+  useEffect(() => {
+    const fetchAssociations = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/association");
+        console.log("Associations data:", response.data);  // Ver qué datos llegan exactamente
+        setAssociations(response.data);
+      } catch (error) {
+        console.error("Error fetching associations", error);
+      }
+    };
+
+    fetchAssociations();
+  }, []);
 
   return (
     <>
@@ -21,7 +48,32 @@ const Associations: React.FC<{ name: string }> = ({ name }) => {
           <IonTitle>{name}</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent class="ion-padding">Associations</IonContent>
+      <IonContent class="ion-padding">
+        {associations.map((association) => (
+          <Card key={association.id} style={{ marginBottom: '20px' }}>
+            <CardMedia
+              component="img"
+              style={{ height: '140px', objectFit: 'contain' }} // Establecer altura y ajuste de objeto
+              image={association.logo}
+              alt={`Logo of ${association.logo}`}
+            />
+            <CardContent>
+              <Typography variant="h5" component="div">
+                {association.name}
+              </Typography>
+              <Typography variant="body2">
+                {association.miniDescription}
+              </Typography>
+              <Typography color="textSecondary">
+                {association.description}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Learn More</Button>
+            </CardActions>
+          </Card>
+        ))}
+      </IonContent>
     </>
   );
 };
